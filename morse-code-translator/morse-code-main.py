2542ff234d0329da6f842bd2351e8465697a83ca
BrodyGill <123396481+BrodyGill@users.noreply.github.com>
2024-12-15 19:58:54 -0600

    added more code to both files

--- morse-code-translator/morse-code-main.py
+++ morse-code-translator/morse-code-main.py
@@ -1,6 +1,6 @@
-import morse-code-translator.py 
+from morse-code-translator.py import *
 
 def main():
     print("Morse code translator.")
     morse = input("Enter morse code: ")
-    
+    translate(morse)
--- morse-code-translator/morse-code-translator.py
+++ morse-code-translator/morse-code-translator.py
@@ -2,6 +2,12 @@ import re
 
 def translate(string1: str):
     str1 = string1
-    sp = len(str1.findall(" "))
+    sp = len(list(map(str, re.findall(" ", str1))))
     
-
+def morseToText(s2, spaces1):
+    str2 = s2
+    sp1 = spaces1
+    al = ""
+    #ql = use regex to get text ending at a space 
+    #for i in sp1:
+        
