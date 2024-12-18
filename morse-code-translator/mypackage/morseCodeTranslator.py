9dad0e7580138a11fca3fdc468716cc0a6713a0d
BrodyGill <123396481+BrodyGill@users.noreply.github.com>
2024-12-18 15:23:45 -0600

    fixed morseCodeTranslater methods

--- morse-code-translator/mypackage/morseCodeTranslator.py
+++ morse-code-translator/mypackage/morseCodeTranslator.py
@@ -1,27 +1,54 @@
 import re
 
 class translator():
-    
+
     def morseToText(str2):
         s2 = str2
-        ms = ""
+        ms = []
+        h = " "
         ls2 = list(s2)
         for i in range(0,len(s2)):
-            if ls2[i] ==  " " and ls2[i-1] == " " and i>0:
+            #if ls2[i] ==  " " and i>0 and ls2[i-1] == " ":
+            #if ls2[i]    
+             #   ms+=" "
+              #  continue
+            if ls2[i] == " ":
                 ms+=" "
                 continue
-            elif ls2[i] == " ":
-                continue
-            if ls2[i]=="-" or ls2[i]==".":
+            elif ls2[i]=="-" or ls2[i]==".":
                 ms+=ls2[i]
                 continue  
-        
+        def Focc(e, sr):
+            el = list(e)
+            se = sr
+            nl = []
+            for p in range(0,len(el)):
+                if(el[p]==se):
+                    nl.append(p)
+            return nl
+        def decode(lang):
+        #use switch to decode morse to readable text
+            l = lang
+            d = []
+            sp=Focc(l, " ")
+            for t in range(0,len(l)):
+                for g in range(0,len(sp)):
+                    if t>0 and t==sp[g] and t-1==sp[g]:
+                        d.append(" ")
+                    
+
+            return 
+        return 
+        #if(l==)
+
+        #decode morse to text then merge indices its easier
     
     #got the decipher down
+
+        
+
     
-    def switch(lang):
-        lms = list(lang)
-        for j in range(0,len(lang)):
-            if lms[j] == 
+    
+
 
         
--- morse-code-translator/test.py
+++ morse-code-translator/test.py
@@ -7,5 +7,24 @@ test = input("test: ")
 
 #ns = re.findall(r"\s", test)
 #jb = list(test)
-qb = re.findall("^-", test)
-print(qb)
+#qb = re.findall("^-", test)
+def findList(s):
+    ls = list(s)
+    nst=""
+    x=0
+    for i in range(0,len(ls)):
+        if ls[i]=="-":
+            x+=1
+            nst+=ls[i]
+        if ls[i]==".":
+            x+=1
+            nst+=ls[i]
+        if ls[i]==" ":
+            x+=1
+    if(x!=len(ls)):
+        return "ERROR: Incorrect input ; Only use .'s and -'s"
+    lls = re.split("\s", ls)
+    wls = re.findall("\s", ls)
+
+
+print()
